75990198d429684c3701b8a02b35982aa995ef0f
Kristian Skov <kris701kj@gmail.com>
2018-07-18 20:01:32 +0200

    Removes more unnecessary files

--- Source Code/ArduLED (Arduino)/ArduLED (Arduino)/Debug/Adafruit_NeoPixel/esp8266.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\kris7\AppData\Local\Temp\VMBuilds\ArduLED\ Arduino\nano_atmega328\Debug\Adafruit_NeoPixel\esp8266.c.o: \
- C:\Users\kris7\Documents\Arduino\libraries\Adafruit_NeoPixel\esp8266.c
--- Source Code/ArduLED (Arduino)/ArduLED (Arduino)/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\kris7\AppData\Local\Temp\VMBuilds\Visualiser\ 1.5\ (Arduino)\nano_atmega328\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Enterprise\Common7\IDE\Extensions\pd4xo3tv.r1p\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
